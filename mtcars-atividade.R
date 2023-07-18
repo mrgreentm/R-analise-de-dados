83dba5e8e64c53021c411fcc40985a284792e695
joao jacinto <joaonetotfm@gmail.com>
2023-07-17 21:35:22 -0300

    feature: calculando valor maximo e minimo de uma coluna

---
+++ mtcars-atividade.R
@@ -0,0 +1,26 @@
+require(car)
+require(dplyr)
+
+## carregando base de dados (o csv)
+data <- read.csv('mtcars.csv', sep = ';', dec = ',', encoding = "UTF-8")
+
+## mostrar os dados em tabela
+View(data)
+
+## exibir as variaveis
+glimpse(data)
+
+## Com a coluna mpg que consta na base, calcular: mínimo e o máximo valor, média,
+## mediana, desvio padrão, os quartis, construir o histograma e o boxplot e interpretar
+## os resultados.
+
+
+## pegando valor maximo da coluna mpg
+coluna_mpg <- data$mpg
+maximo <- max(coluna_mpg)
+maximo
+
+## pegando o valor mínimo da coluna mpg
+minimo <- min(coluna_mpg)
+minimo
+
