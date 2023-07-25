159f8821b43d8c9e68196b0b1e53f369da1245fc
joao jacinto <joaonetotfm@gmail.com>
2023-07-24 21:19:21 -0300

    feature: grafico de barras analise dados enem

--- atividade-2.R
+++ atividade-2.R
@@ -11,3 +11,15 @@ View(data_enem)
 ## exibir as variaveis
 glimpse(data_imc)
 glimpse(data_enem)
+
+# a) Com algumas variáveis QUALITATIVAS, elaborar gráficos de barras e comentar
+# os resultados.
+# utilizaremos TP_SEXO e TP_LINGUA
+coluna_tp_sexo <- data_enem$TP_SEXO
+coluna_tp_lingua <- data_enem$TP_LINGUA
+matriz_frequencias <- table(coluna_tp_sexo, coluna_tp_lingua)
+# Passo 2: Criar os gráficos de barras sexo x lingua
+par(mfrow = c(1, 2))  # Organiza os gráficos em uma linha com 2 colunas
+barplot(matriz_frequencias, beside = TRUE, legend.text = TRUE, col = rainbow(nrow(matriz_frequencias)), main = "Sexo x Lingua", xlab = "Linguas", ylab = "Frequência")
+# percebe-se que,os que escolhem a lingua estrangeira ESPANHOL, sao em grande maioria, mulheres
+# por outro lado, dentre os que escolhem inglês, mulheres e homens são quase que iguais em número, tendo as mulheres pequena maioria
